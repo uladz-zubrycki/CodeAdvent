92e0005e0095ba2ede61e351471ad79691d7e18f
v.zubritsky <v.zubritsky@gmail.com>
2017-12-05 17:39:40 +0300

    implement 2.2

--- 2.fsx
+++ 2.fsx
@@ -1,5 +1,4 @@
 open System
-open System.IO
 
 (* Part I*)
 let calculateChecksum1 (spreadsheet: int list list) = 
@@ -17,3 +16,28 @@ calculateChecksum1
     [[5; 1; 9; 5]
      [7; 5; 3]
      [2; 4; 6; 8]] // 18
+
+(* Part II *)
+let calculateChecksum2 (spreadsheet: int list list) = 
+    spreadsheet
+    |> List.sumBy (fun row -> 
+        let pairs = seq {
+            for i in 0..row.Length - 1 do
+                for j in i + 1..row.Length - 1 do
+                    yield row.[i], row.[j] }
+
+        pairs 
+        |> Seq.map (fun (fst, snd) -> 
+            match Math.DivRem(fst, snd) with
+            | div, 0 -> Some(div)
+            | _, _ ->
+                match Math.DivRem(snd, fst) with 
+                | div, 0 -> Some(div)
+                | _, _ -> None )
+        |> Seq.find Option.isSome 
+        |> Option.get)
+
+calculateChecksum2
+    [[5; 9; 2; 8]
+     [9; 4; 7; 3]
+     [3; 8; 6; 5]] // 9
